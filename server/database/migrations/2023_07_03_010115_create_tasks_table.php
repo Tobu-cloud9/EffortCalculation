a36bb49aba83ce0cb6f2af73866a88dffd490b77
Tobu-cloud9 <kanmi132255817@gmail.com>
2023-07-06 01:05:35 +0000

    fix(migration): fix column

--- server/database/migrations/2023_07_03_010115_create_tasks_table.php
+++ server/database/migrations/2023_07_03_010115_create_tasks_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('task_id');
             $table->foreignId('project_id');
-            $table->string('task_name');
+            $table->string('title', 100);
+            $table->text('content');
             $table->date('start_at');
             $table->date('goal_at');
             $table->date('end_at');
